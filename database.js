6b8b0bda028eb355b41fe49cc467891987bf2f19
Dominicoso <dominicoso@gmail.com>
2022-04-30 19:15:29 -0400

    CHANGED SOME TYPES WHEN CREATING TABLE

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ if (row === undefined) {
     console.log('Log database missing. Creating log database...')
 
     const sqlInit = `
-        CREATE TABLE accesslog ( remoteaddr ip, remoteuser user, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
+        CREATE TABLE accesslog ( remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR );
     `
 
 
